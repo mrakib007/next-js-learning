c60684ed08f82640148d6d6c82974dd6858c1ed7
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-01 01:09:22 +0600

    File based routing exploring with dynamic routing

---
+++ second-next-js-app/pages/blogPost/[sno].js
@@ -0,0 +1,9 @@
+const { useRouter } = require("next/router")
+
+const Post = () =>{
+    const router = useRouter();
+    const {sno} = router.query;
+
+    return <p>Post: {sno} </p>
+}
+export default Post;
