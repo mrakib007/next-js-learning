882bb3a77be5e9d454bbd10cfb1e8d25f04a8b59
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-02 11:46:15 +0600

    styled jsx learning

---
+++ huntingcoder/components/dummy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <>
+    <style jsx global>
+        {`
+        .dummy{
+            background: yellow;
+        }
+        `}
+    </style>
+    <div className='dummy'>
+      dummy
+    </div>
+    </>
+  )
+}
+
+export default Dummy;
