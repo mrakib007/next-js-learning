06a9b5d27f02c57f7b8bb0e02bffeba1781bb1b2
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-02 14:46:56 +0600

    Added blogpost

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 const Blog = () => {
   return (
-    <div>
-      This is blog page.
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.myDiv}>
+        <div className={styles.blogItem}>
+          <Link href={'/blogpost/learn-javascript'}>
+          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022</h3>
+          </Link>
+          <p className={styles.blogItemp}>JavaScript is a language used for designing web pages</p>
+        </div>
+        <div className={styles.blogItem}>
+          <Link href={'/blogpost/learn-javascript'}>
+          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022</h3>
+          </Link>
+          <p className={styles.blogItemp}>JavaScript is a language used for designing web pages</p>
+        </div>
+        <div className={styles.blogItem}>
+          <Link href={'/blogpost/learn-javascript'}>
+          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022</h3>
+          </Link>
+          <p className={styles.blogItemp}>JavaScript is a language used for designing web pages</p>
+        </div>
+        </div>
+      </main> 
     </div>
-  )
-}
+  );
+};
 
 export default Blog;
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import React from 'react'
+import styles from "../../styles/BlogPost.module.css";
 
 const Slug = () => {
     const router = useRouter();
@@ -7,7 +8,14 @@ const Slug = () => {
 
   return (
     <div>
-      {slug}
+      <main className={styles.main}>
+      <h1>Title Of the page {slug}</h1>
+      <hr />
+    <div>
+     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, quia voluptatibus id, corrupti, nemo numquam velit animi voluptatem illum veniam eos eligendi corporis! Nostrum quo ad eius quae sapiente nihil commodi dolorem perferendis eligendi sunt? Quo nihil doloribus error laborum quidem,
+      possimus in iste praesentium incidunt. Minima suscipit quaerat illo?
+    </div>
+    </main>
     </div>
   )
 }
