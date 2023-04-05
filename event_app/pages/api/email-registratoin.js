efeeea83945818c88bcf55cee471a930398ce3af
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-05 23:46:48 +0600

    Adding post methods for posting the email id's for individual event registration

---
+++ event_app/pages/api/email-registratoin.js
@@ -0,0 +1,9 @@
+export default function handler (req,res){
+    const {method} = req;
+    if(method == "POST"){
+        const {email,eventId} = req.body;
+
+        res.status(200).json({message:`You hav been registered 
+        successfully with the email: ${email} ${eventId}`})
+    }
+}
--- event_app/src/components/events/singleEvent.jsx
+++ event_app/src/components/events/singleEvent.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Image from 'next/image';
-const SingleEvent = ({data}) => {
+import React, { useRef } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+const SingleEvent = ({ data }) => {
+  const inputEmail = useRef();
+  const router = useRouter();
+
+  const onSubmit = async (event) =>{
+    event.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+
+    try{
+      //POST fetch request
+      //body emailValue and the eventId
+      const response = await fetch('/api/email-registration',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email:emailValue,eventId})
+      });
+      if(!response.ok) throw new Error(`Error: ${response.status}`)
+      const data = await response.json();
+    }catch(error){
+      console.log(error + " " + "Error")
+    }
+  }
   return (
     <div>
-      <Image src={data.image} width={1000} height={500} alt={data.title}/>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
       <h1>{data.title}</h1>
       <p>{data.description}</p>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="Get's Started For This Event"></label>
+        <input ref={inputEmail} type="email" name="" id="email" placeholder="please insert your email here" />
+        <button type="button">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SingleEvent
+export default SingleEvent;
