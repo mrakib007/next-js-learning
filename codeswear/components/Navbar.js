2248862e048bdc364708e57f9ed60099a44f899f
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-15 11:10:21 +0600

    adding cart design

--- codeswear/components/Navbar.js
+++ codeswear/components/Navbar.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-// import React from "react";
-import {AiOutlineShoppingCart} from 'react-icons/ai';
+import React, { useRef } from "react";
+import {AiFillCloseCircle, AiOutlineShoppingCart} from 'react-icons/ai';
 
 const Navbar = () => {
+  const toggleCart = () =>{
+
+  }
+  const ref = useRef();
   return (
     <div className="flex md:flex-row md:justify-start flex-col justify-center items-center py-2 shadow-md">
       <div className="logo mx-5">
@@ -19,6 +23,15 @@ const Navbar = () => {
       </div>
       <div className="cart absolute right-0 mx-5 top-4">
       <AiOutlineShoppingCart className="text-xl md:text-2xl"/>
+      </div>
+
+      <div ref={ref} className="sidebar absolute top-0 right-0 bg-pink-100 p-10">
+        <h2 className="font-bold text-xl">Shopping Cart</h2>
+        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle/></span>
+        <ol>
+          <li>
+            <span>T-Shirt - Wear The Code</span></li>
+        </ol>
       </div>
     </div>
   );
