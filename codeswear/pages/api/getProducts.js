dfc43dd80ca89651ea78e3d24a93628493675b2c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-21 23:36:49 +0600

    adding codes for more precise filtering

--- codeswear/pages/api/getProducts.js
+++ codeswear/pages/api/getProducts.js
@@ -4,7 +4,26 @@ import Product from "@/models/Product";
 
 const handler = async (req, res) =>{
     let products = await Product.find();
-    res.status(200).json({products});     
+    let tShirts = {};
+    for(let item of products){
+        if(item.title in tShirts){
+           if(!tShirts[item.title].color.includes(item.color) && 
+           item.availableQty > 0){
+            tShirts[item.title].color.push(item.color);
+           }
+           if(!tShirts[item.title].size.includes(item.size) && 
+           item.availableQty > 0){
+            tShirts[item.title].size.push(item.size);
+           }
+        }else{
+            tShirts[item.title] = JSON.parse(JSON.stringify(item));
+            if(item.availableQty > 0){
+                tShirts[item.title].color = [item.color];
+                tShirts[item.title].size = [item.size];
+            }
+        }
+    }
+    res.status(200).json({tShirts});     
 }
 export default connectDb(handler);
   
